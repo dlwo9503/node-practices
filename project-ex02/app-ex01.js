760fef4a1b7d0ace4542515cf007c40c8fba45e1
jaes <dlwo9503@naver.com>
2021-07-09 12:54:58 +0900

    모듈 테스트

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,8 @@
+/**
+ * jaeseong-math.js 파일 모듈 테스트
+ */
+const jsMath = require('../jaeseong-math/jaeseong-math');
+
+console.log(jsMath.sum(1,2,3,4));
+console.log(jsMath.max(-10,2,3,40));
+console.log(jsMath.min(-30,-10,2,3,40));
---
+++ project-ex02/app-ex02.js
@@ -0,0 +1,5 @@
+const jsMath = require('jaeseong-math');
+
+console.log(jsMath.sum(1,2,3,4));
+console.log(jsMath.max(-10,2,3,40));
+console.log(jsMath.min(-30,-10,2,3,40));
