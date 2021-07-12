feee2293e77801e2e9dde385f85983a293ef8710
jaes <dlwo9503@naver.com>
2021-07-12 15:01:28 +0900

    express 연습#1

--- helloweb-ex01/app01.js
+++ helloweb-ex01/app01.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const port = 8080;
-const server = http.createServer(function(req, resp){
-    resp.writeHead(200, {
+const port = 8080; // port number
+const server = http.createServer(function(req, resp){ // 매개변수로 (요청, 응답)
+    resp.writeHead(200, { // Header
         'Content-Type': 'text/html'
     });
-    resp.end('<h1>Hello Web</h1>');
+    resp.end('<h1>Hello Web</h1>'); // Body
 })
 
-server.listen(port, function(){
+server.listen(port, function(){ // port number bind
     console.log(`Http Server running on port ${port}`);
 })
